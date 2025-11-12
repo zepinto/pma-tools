536613f3daa845dd495fbb074f7b6c5dd8b2d7dd
Jose Pinto <zepinto@gmail.com>
2025-11-12 09:33:04 +0000

    Added methods for converting LogMarkers.

--- contacts/src/main/java/pt/omst/contacts/ContactUtils.java
+++ contacts/src/main/java/pt/omst/contacts/ContactUtils.java
@@ -5,12 +5,25 @@
 //***************************************************************************
 package pt.omst.contacts;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
+import pt.lsts.neptus.mra.LogMarker;
+import pt.lsts.neptus.mra.SidescanLogMarker;
+import pt.omst.WGS84Utilities;
 import pt.omst.neptus.core.LocationType;
+import pt.omst.neptus.core.SystemPositionAndAttitude;
+import pt.omst.neptus.sidescan.ISidescanLine;
+import pt.omst.neptus.sidescan.SidescanParser;
+import pt.omst.neptus.sidescan.SidescanParserFactory;
 import pt.omst.neptus.util.StreamUtil;
 import pt.omst.neptus.util.ZipUtils;
 import pt.omst.rasterlib.Annotation;
@@ -18,16 +31,16 @@ import pt.omst.rasterlib.AnnotationType;
 import pt.omst.rasterlib.Contact;
 import pt.omst.rasterlib.Converter;
 import pt.omst.rasterlib.IndexedRaster;
+import pt.omst.rasterlib.IndexedRasterCreator;
 import pt.omst.rasterlib.IndexedRasterUtils.RasterContactInfo;
 import pt.omst.rasterlib.MeasurementType;
 import pt.omst.rasterlib.Observation;
 import pt.omst.rasterlib.SensorInfo;
 import pt.omst.rasterlib.contacts.CompressedContact;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 public class ContactUtils {
+
     public static RasterContactInfo getContactInfo(CompressedContact contact) {
         Contact c = contact.getContact();
         RasterContactInfo info = new RasterContactInfo();
@@ -62,28 +75,30 @@ public class ContactUtils {
                         sssObservation.getRasterFilename());
                 String rasterJson = StreamUtil.copyStreamToString(is);
                 raster = Converter.IndexedRasterFromJsonString(rasterJson);
-                raster.getSamples().sort( (o1, o2) -> o1.getTimestamp().compareTo(o2.getTimestamp()));
+                raster.getSamples().sort((o1, o2) -> o1.getTimestamp().compareTo(o2.getTimestamp()));
                 sensorInfo = raster.getSensorInfo();
                 info.setMinRange(sensorInfo.getMinRange());
                 info.setMaxRange(sensorInfo.getMaxRange());
 
                 info.setStartTimeStamp(raster.getSamples().getFirst().getTimestamp().toInstant().toEpochMilli());
                 info.setEndTimeStamp(raster.getSamples().getLast().getTimestamp().toInstant().toEpochMilli());
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                //System.out.println(info.label+" is from "+sdf.format(new Date(info.startTimeStamp))+" to "+sdf.format(new Date(info.endTimeStamp)));
+                
                 if (boxAnnotation != null) {
-                    double newMinRange = info.getMinRange() + boxAnnotation.getNormalizedX() * (info.getMaxRange() - info.getMinRange());
-                    double newMaxRange = info.getMinRange() + boxAnnotation.getNormalizedX2() * (info.getMaxRange() - info.getMinRange());
-                    double newStartTimestamp = info.getStartTimeStamp() + boxAnnotation.getNormalizedY() * (info.getEndTimeStamp() - info.getStartTimeStamp());
-                    double newEndTimestamp = info.getStartTimeStamp() + boxAnnotation.getNormalizedY2() * (info.getEndTimeStamp() - info.getStartTimeStamp());
+                    double newMinRange = info.getMinRange()
+                            + boxAnnotation.getNormalizedX() * (info.getMaxRange() - info.getMinRange());
+                    double newMaxRange = info.getMinRange()
+                            + boxAnnotation.getNormalizedX2() * (info.getMaxRange() - info.getMinRange());
+                    double newStartTimestamp = info.getStartTimeStamp()
+                            + boxAnnotation.getNormalizedY() * (info.getEndTimeStamp() - info.getStartTimeStamp());
+                    double newEndTimestamp = info.getStartTimeStamp()
+                            + boxAnnotation.getNormalizedY2() * (info.getEndTimeStamp() - info.getStartTimeStamp());
                     info.setBoxAnnotation(boxAnnotation);
                     info.setMinRange(newMinRange);
                     info.setMaxRange(newMaxRange);
                     info.setStartTimeStamp((long) newStartTimestamp);
                     info.setEndTimeStamp((long) newEndTimestamp);
                 }
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 log.error("Error reading raster file: " + e.getMessage());
             }
         } else {
@@ -91,5 +106,232 @@ public class ContactUtils {
             info.setStartTimeStamp(info.getEndTimeStamp());
         }
         return info;
+    }
+
+    public static Contact convert(LogMarker marker, SidescanParser parser, File outputFolder) throws Exception {
+        Collection<ISidescanLine> lines = parser.getLinesAtTime((long)marker.getTimestamp());
+        SystemPositionAndAttitude pose = null;
+        if (lines.isEmpty()) {
+            log.warn("No sidescan lines found for marker at time " + marker.getTimestamp());
+        }
+        ISidescanLine line = lines.iterator().next();
+        pose = line.getState();
+        return convert(marker, pose, parser, outputFolder);
+    }
+
+    public static Collection<Contact> convertContacts(File folder) {
+        if (folder == null) {
+            return null;
+        }
+        File outputFolder = new File(folder, "contacts");
+        SidescanParser parser = SidescanParserFactory.build(folder);
+        Collection<LogMarker> markers = LogMarker.load(folder);
+        Collection<Contact> contacts = new ArrayList<>();
+        for (LogMarker marker : markers) {
+            try {
+                Contact contact = convert(marker, parser, outputFolder);
+                if (contact != null) {
+                    contacts.add(contact);
+                }
+            } catch (Exception e) {
+                log.error("Error converting marker " + marker.getLabel(), e);
+            }
+        }
+        return contacts;
+    }
+
+    /**
+     * Convert a log marker to a contact
+     * 
+     * @param marker       The marker to convert
+     * @param log          The log group to get the system name from
+     * @param pose         The pose of the vehicle at the time of the marker
+     * @param parser       The parser to use to get the raster
+     * @param outputFolder The folder to save the raster to
+     * @return The contact for the given marker
+     * @throws Exception If an error occurs while converting the marker
+     */
+    public static Contact convert(LogMarker marker, SystemPositionAndAttitude pose, SidescanParser parser,
+            File outputFolder) throws Exception {
+        if (marker.getParent() != null)
+            return null;
+        Contact contact = new Contact();
+        contact.setLabel(marker.getLabel());
+        contact.setLatitude(Math.toDegrees(marker.getLatRads()));
+        contact.setLongitude(Math.toDegrees(marker.getLonRads()));
+        contact.setObservations(new ArrayList<>());
+        if (pose != null)
+            contact.setDepth(pose.getDepth() + pose.getAltitude());
+        ArrayList<LogMarker> thisAndChildren = new ArrayList<>();
+        thisAndChildren.add(marker);
+        thisAndChildren.addAll(marker.getChildren());
+        for (LogMarker child : thisAndChildren) {
+            if (child instanceof SidescanLogMarker)
+                contact.getObservations()
+                        .addAll(getSidescanObservations((SidescanLogMarker) child, pose, parser, outputFolder));
+            else
+                contact.getObservations().addAll(getGenericObservations(child, pose));
+        }
+
+        return contact;
+    }
+
+    public static String sanitize(String input) {
+        return input.replaceAll("[^a-zA-Z0-9.-]", "_");
+    }
+
+    public static void zipFolderAndDelete(File folder) {
+        try {
+            File zipFile = new File(folder.getParentFile(), folder.getName() + ".zct");
+            ZipUtils.zipDir(zipFile.getAbsolutePath(), folder.getAbsolutePath());
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    file.delete();
+                }
+            }
+            folder.delete();
+        } catch (Exception e) {
+            log.error("Error zipping folder", e);
+        }
+    }
+
+    /**
+     * Get the observation for a sidescan marker and subsystem
+     * 
+     * @param marker       The sidescan marker to get the observation for
+     * @param pose         The pose of the vehicle at the time of the marker
+     * @param log          The log group, used to get the system name
+     * @param parser       The parser to use to get the raster
+     * @param subsystem    The subsystem to get the observation for
+     * @param outputFolder The folder to save the raster to
+     * @return The observation for the given marker and subsystem
+     */
+    public static Observation getSidescanObservation(SidescanLogMarker marker, SystemPositionAndAttitude pose,
+            SidescanParser parser, int subsystem, File outputFolder) {
+        SensorInfo info = new SensorInfo();
+        File rasterFile = new File(outputFolder, "marker_" + (long) marker.getTimestamp() + "_" + subsystem + ".json");
+        IndexedRasterCreator creator = new IndexedRasterCreator(
+                rasterFile,
+                info);
+        int margin = 50;
+        creator.export(marker, parser, subsystem, margin);        
+        BufferedImage exported = creator.getImage();
+
+        double boxX = margin, boxY = margin, boxWidth = exported.getWidth() - margin * 2,
+                boxHeight = exported.getHeight() - margin * 2;
+        boxX /= exported.getWidth();
+        boxY /= exported.getHeight();
+        boxWidth /= exported.getWidth();
+        boxHeight /= exported.getHeight();
+
+        Annotation boxSize = new Annotation();
+        boxSize.setAnnotationType(AnnotationType.MEASUREMENT);
+        boxSize.setMeasurementType(MeasurementType.BOX);
+        boxSize.setNormalizedX(boxX);
+        boxSize.setNormalizedY(boxY);
+        boxSize.setNormalizedX2(boxX + boxWidth);
+        boxSize.setNormalizedY2(boxY + boxHeight);
+        boxSize.setTimestamp(OffsetDateTime.ofInstant(marker.getDate().toInstant(), ZoneId.systemDefault()));
+        boxSize.setUserName(System.getProperty("user.name"));
+
+        log.debug("Box is at " + boxX + ", " + boxY + " with width " + boxWidth + " and height " + boxHeight);
+
+        // Add the observation for the marker itself
+        Observation observation = new Observation();
+        observation.setDepth(pose.getPosition().getDepth());
+        observation.setLatitude(Math.toDegrees(marker.getLatRads()));
+        observation.setLongitude(Math.toDegrees(marker.getLonRads()));
+        observation.setTimestamp(OffsetDateTime.ofInstant(marker.getDate().toInstant(), ZoneId.systemDefault()));
+        observation.setUserName(System.getProperty("user.name"));
+        observation.setSystemName("rasterfall");
+        observation.setRasterFilename(rasterFile.getName());
+        observation.setAnnotations(new ArrayList<>());
+        observation.getAnnotations().add(boxSize);
+        return observation;
+    }
+
+    /**
+     * Get the horizontal pixels per meter for a raster and image
+     * 
+     * @param raster The raster to get the pixels per meter for
+     * @param image  The image to get the pixels per meter for
+     * @return The horizontal pixels per meter for the raster and image
+     */
+    public static double getHorizontalPixelsPerMeter(IndexedRaster raster, BufferedImage image) {
+        double pixelWidth = image.getWidth();
+        double rasterWidth = raster.getSensorInfo().getMaxRange() - raster.getSensorInfo().getMinRange();
+        return pixelWidth / rasterWidth;
+    }
+
+    /**
+     * Get the vertical pixels per meter for a raster and image based on the first
+     * and last samples.
+     * Assumes the vehicle is moving in a straight line between the first and last
+     * samples.
+     * 
+     * @param raster The raster to get the pixels per meter for
+     * @param image  The image to get the pixels per meter for
+     * @return The vertical pixels per meter for the raster and image, based on the
+     *         first and last sample positions
+     */
+    public static double getVerticalPixelsPerMeter(IndexedRaster raster, BufferedImage image) {
+        double pixelHeight = image.getHeight();
+        double verticalDistance = WGS84Utilities.distance(
+                raster.getSamples().getFirst().getPose().getLatitude(),
+                raster.getSamples().getFirst().getPose().getLongitude(),
+                raster.getSamples().getLast().getPose().getLatitude(),
+                raster.getSamples().getLast().getPose().getLongitude());
+        return pixelHeight / verticalDistance;
+    }
+
+    /**
+     * Get the observations for a sidescan marker
+     * 
+     * @param marker       The sidescan marker to get the observations for
+     * @param pose         The pose of the vehicle at the time of the marker
+     * @param log          The log group, used to get the system name
+     * @param parser       The parser to use to get the raster
+     * @param outputFolder The folder to save the raster to
+     * @return The observations for the given marker
+     * @throws Exception If an error occurs while getting the observations
+     */
+    public static ArrayList<Observation> getSidescanObservations(SidescanLogMarker marker,
+            SystemPositionAndAttitude pose, SidescanParser parser, File outputFolder) throws Exception {
+        ArrayList<Observation> observations = new ArrayList<>();
+        ArrayList<Integer> subsystems = parser.getSubsystemList();
+        for (int subsystem : subsystems) {
+            observations.add(getSidescanObservation(marker, pose, parser, subsystem, outputFolder));
+        }
+
+        return observations;
+    }
+
+    /**
+     * Get the observations for a generic marker
+     * 
+     * @param marker The marker to get the observations for
+     * @param pose   The pose of the vehicle at the time of the marker
+     * @param log    The log group, used to get the system name
+     * @return The observations for the given marker
+     */
+    public static ArrayList<Observation> getGenericObservations(LogMarker marker, SystemPositionAndAttitude pose) {
+        ArrayList<Observation> observations = new ArrayList<>();
+        Observation observation = new Observation();
+        observation.setDepth(pose.getPosition().getDepth());
+        observation.setLatitude(Math.toDegrees(marker.getLatRads()));
+        observation.setLongitude(Math.toDegrees(marker.getLonRads()));
+        observation.setTimestamp(OffsetDateTime.ofInstant(marker.getDate().toInstant(), ZoneId.systemDefault()));
+        observation.setUserName(System.getProperty("user.name"));
+        observation.setSystemName("rasterfall");
+        observations.add(observation);
+
+        return observations;
+    }
+
+    public static void main(String[] args) {
+        File folder = new File("/LOGS/153320_03-fat-sidescan-depth/");
+        Collection<Contact> contacts = convertContacts(folder);
+        log.info("Converted " + contacts.size() + " contacts.");
     }
 }
